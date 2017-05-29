d6afcaa8146b53264b93e252f9bfe69ca2a5855c
Douglas G. Allen <kb9agt@gmail.com>
2017-05-28 23:31:20 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,24 +1,29 @@
 #!/usr/bin/ruby
 
+require 'date'
 puts ARGV
 
-def time_by_angle angle 
-    raise "Angle less then 0 in method time_by_angle" if angle < 0
-    buf = angle
-    while buf > 360
-        buf -= 360
-    end
-    return buf*240
+def time_by_angle(angle)
+  # raise 'Angle less then 0 in method time_by_angle' if angle < 0
+  buf = angle
+  buf -= 360 while buf > 360
+  buf * 240
 end
 
-def time_shift day
-    b = Math::PI*(day - 81)/365
-
-    return 7.53 * Math.cos(b) + 1.5 * Math.sin(b) - 9.87 * Math.sin(2*b)
+def time_shift(day)
+  bias = 2 * Math::PI / 364.0 * (day - 81)
+  9.87 * Math.sin(2 * bias) - 7.53 * Math.cos(bias) - 1.5 * Math.sin(bias)
 end
 
-def local_mean_solar_time utc, longtitude
-    day = utc.yday
+def local_mean_solar_time(utc, longtitude)
+  day = utc.yday
 
-    return utc + time_by_angle(longtitude) + time_shift(day)
+  utc - time_by_angle(longtitude) + time_shift(day)
 end
+
+lng = -88.75
+puts time_by_angle(lng)
+t = Time.now.utc
+p mean_noon_utc = Time.new(t.year, t.month, t.day, 12, 0, 0, "+00:00")
+puts time_shift(Date.today.yday)
+puts local_mean_solar_time(mean_noon_utc, lng)
